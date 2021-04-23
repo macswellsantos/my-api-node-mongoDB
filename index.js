b0ec05f87135f98c16f17508028c7cdfc9b2f155
Mac-Swell Santos <macswell.salviano@gmail.com>
2021-04-23 18:52:22 -0300

    configuração mongoDB Atlas

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { studentRouter } from './routes/studentRouter.js';
+import dotenv from 'dotenv';
 
 const app = express();
-
-require('dotenv').config();
+dotenv.config();
 
 (async () => {
   try {
     //'mongodb://localhost/grades'
-    await mongoose.connect(
-      `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@api.pb75e.mongodb.net/grades?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const ATLASURL = `mongodb+srv://${process.env.USERDB}:${process.env.PWDDB}@api.pb75e.mongodb.net/grades?retryWrites=true&w=majority`;
+    console.log(ATLASURL);
+    //prettier-ignore
+    await mongoose.connect(ATLASURL, {
+      useNewUrlParser: true, useUnifiedTopology: true
+    });
     console.log('Conectado ao MongoDB');
   } catch (err) {
-    console.log('erro ao iniciar MongoDB: ' + err);
+    console.log('erro ao iniciar MongoDB: ' + err.message);
   }
 })();
 
